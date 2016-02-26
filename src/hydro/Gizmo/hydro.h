3b54ae245db04ca255f0b87bc1c5187a19931a85
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-02-26 15:45:30 +0000

    Added missing variables and hydro routines to compile

--- src/hydro/Gizmo/hydro.h
+++ src/hydro/Gizmo/hydro.h
@@ -590,3 +590,19 @@ __attribute__((always_inline))
   p->conserved.energy = p->primitives.P / (const_hydro_gamma - 1.) * volume;
 #endif
 }
+
+// MATTHIEU
+__attribute__((always_inline))
+    INLINE static void hydro_end_density(struct part* p, float time) {}
+__attribute__((always_inline)) INLINE static void hydro_prepare_force(
+    struct part* p, struct xpart* xp, float time) {}
+__attribute__((always_inline)) INLINE static void hydro_predict_extra(
+    struct part* p, struct xpart* xp, float t0, float t1) {}
+__attribute__((always_inline))
+    INLINE static void hydro_end_force(struct part* p) {}
+__attribute__((always_inline)) INLINE static void hydro_kick_extra(
+    struct part* p, struct xpart* xp, float dt, float half_dt) {}
+__attribute__((always_inline))
+    INLINE static float hydro_get_internal_energy(struct part* p) {
+  return 0.f;
+}
