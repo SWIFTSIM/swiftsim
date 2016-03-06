a552e2703e635181a71da7ec51d52363d9898a4d
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-03-06 21:50:00 +0000

    Compiling version

--- src/single_io.c
+++ src/single_io.c
@@ -409,9 +409,8 @@ void read_ic_single(char* fileName, double dim[3], struct part** parts, struct g
 	  hydro_read_particles(h_grp, *Ngas, *Ngas, 0, *parts);
 	else if(DARKMATTER == ptype)
 	  {
-		 int i, ic;
 		 darkmatter_read_particles(h_grp, *Ndm, *Ndm, 0, *gparts);
-		 for(i=0; i<*Ndm; i++)
+		 for(int i=0; i<*Ndm; i++)
 			(*gparts)[i].id = -abs( (*gparts)[i].id );
 		 /* /\* for testing: copy gparts into parts as if we were doing SPH *\/ */
 		 /* *Ngas = *Ndm; */
