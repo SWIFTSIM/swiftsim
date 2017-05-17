dbbbcd1631c2e0897fc264e37a3391665e2755ab
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-05-17 18:48:51 +0100

    Removed debugging code. Updated documentation. Removed unnecessary condition.

--- src/timestep_limiter.h
+++ src/timestep_limiter.h
@@ -22,6 +22,14 @@
 /* Config parameters. */
 #include "../config.h"
 
+/**
+ * @brief Wakes up a particle by rewinding it's kick1 back in time and applying
+ * a new one such that the particle becomes active again in the next time-step.
+ *
+ * @param p The #part to update.
+ * @param xp Its #xpart companion.
+ * @param e The #engine (to extract time-line information).
+ */
 __attribute__((always_inline)) INLINE static integertime_t timestep_limit_part(
     struct part *restrict p, struct xpart *restrict xp,
     const struct engine *e) {
@@ -33,9 +41,11 @@ __attribute__((always_inline)) INLINE static integertime_t timestep_limit_part(
       get_integer_time_end(e->ti_current, p->time_bin);
   const integertime_t old_dti = old_ti_end - old_ti_beg;
 
-  /* The new time-step */
+  /* The new fake time-step the particle will be on */
   const integertime_t new_fake_ti_step =
       get_integer_timestep(e->min_active_bin);
+
+  /* The actual time-step size this particle will use */
   const integertime_t new_ti_beg = old_ti_beg;
   const integertime_t new_ti_end = e->ti_current + new_fake_ti_step;
   const integertime_t new_dti = new_ti_end - new_ti_beg;
@@ -47,56 +57,32 @@ __attribute__((always_inline)) INLINE static integertime_t timestep_limit_part(
         "Incorrect value for old time-step beginning ti_current=%lld, "
         "old_ti_beg=%lld",
         e->ti_current, old_ti_beg);
+
   if (old_ti_end <= e->ti_current)
     error(
         "Incorrect value for old time-step end ti_current=%lld, "
         "old_ti_end=%lld",
         e->ti_current, old_ti_end);
+
   if (new_ti_end > old_ti_end)
     error("New end of time-step longer than old one");
+
   if (new_dti > old_dti) error("New time-step larger than old one");
+
   if (new_fake_ti_step == 0) error("Wakeup call too early");
 #endif
-
-  /* if (p->id == ICHECK) */
-  /*   message( */
-  /*       "ti_current=%lld, ti_drift=%lld ti_kick=%lld, time_bin=%d wakeup=%d "
-   */
-  /*       "new_ti_end=%lld", */
-  /*       e->ti_current, p->ti_drift, p->ti_kick, p->time_bin, p->wakeup, */
-  /*       new_ti_end); */
 
   /* Now we need to reverse the kick1...*/
   kick_part(p, xp, old_ti_beg + old_dti / 2, old_ti_beg, e->timeBase);
 
-  /* if (p->id == ICHECK) */
-  /*   message( */
-  /*       "ti_current=%lld, ti_drift=%lld ti_kick=%lld, time_bin=%d wakeup=%d "
-   */
-  /*       "new_ti_end=%lld", */
-  /*       e->ti_current, p->ti_drift, p->ti_kick, p->time_bin, p->wakeup, */
-  /*       new_ti_end); */
-
   /* ...and apply the new one */
   kick_part(p, xp, new_ti_beg, new_ti_beg + new_dti / 2, e->timeBase);
-
-  /* if (p->id == ICHECK) */
-  /*   message( */
-  /*       "ti_current=%lld, ti_drift=%lld ti_kick=%lld, time_bin=%d wakeup=%d",
-   */
-  /*       e->ti_current, p->ti_drift, p->ti_kick, p->time_bin, p->wakeup); */
 
   /* Remember the old time-bin */
   p->wakeup = p->time_bin;
 
   /* Update the time bin of this particle */
   p->time_bin = e->min_active_bin;
-
-  /* if (p->id == ICHECK) */
-  /*   message( */
-  /*       "ti_current=%lld, ti_drift=%lld ti_kick=%lld, time_bin=%d wakeup=%d",
-   */
-  /*       e->ti_current, p->ti_drift, p->ti_kick, p->time_bin, p->wakeup); */
 
   return new_fake_ti_step;
 }
