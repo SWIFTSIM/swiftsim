356aeb73fe2d56074c042bea75b40d71e0a6d031
lhausamm <loic_hausammann@hotmail.com>
2017-08-29 17:13:59 +0200

    Add index file writing.  - Currently too much copy-paste-modify. Will need to merge all the code when the code is ready  - need to remove a bug in the number of task created before testing this commit

--- src/single_io.h
+++ src/single_io.h
@@ -40,6 +40,9 @@ void write_output_single(struct engine* e, const char* baseName,
                          const struct unit_system* internal_units,
                          const struct unit_system* snapshot_units);
 
+void write_index_single(struct engine* e, const char* baseName,
+			const struct unit_system* internal_units,
+			const struct unit_system* snapshot_units);
 #endif
 
 #endif /* SWIFT_SINGLE_IO_H */
