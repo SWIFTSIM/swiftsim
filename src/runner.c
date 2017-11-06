a7afe66745065baec2d464f5f87529aaa15cf25e
lhausamm <loic_hausammann@hotmail.com>
2017-11-06 16:26:44 +0100

    Remove bug when logger is disabled

--- src/runner.c
+++ src/runner.c
@@ -2163,6 +2163,7 @@ void *runner_main(void *data) {
  */
 void runner_do_logger(struct runner *r, struct cell *c, int timer) {
 
+#ifdef WITH_LOGGER
   const struct engine *e = r->e;
   struct part *restrict parts = c->parts;
   struct xpart *restrict xparts = c->xparts;
@@ -2238,4 +2239,9 @@ void runner_do_logger(struct runner *r, struct cell *c, int timer) {
 
   }
   if (timer) TIMER_TOC(timer_logger);
+
+#else
+  error("Logger disabled, please enable it during configuration");
+#endif
+  
 }
