67d67a9c93ac791c34bcafec4da8ffa099f3ac21
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-05-22 10:03:57 +0100

    Update with utilities changes

--- src/equation_of_state/planetary/tillotson.h
+++ src/equation_of_state/planetary/tillotson.h
@@ -41,7 +41,7 @@
 
 // Tillotson parameters
 struct Til_params {
-  float rho_0, a, b, A, B, E_0, E_iv, E_cv, alpha, beta, eta_min, P_min;
+  float rho_0, a, b, A, B, u_0, u_iv, u_cv, alpha, beta, eta_min, P_min;
   enum eos_planetary_material_id mat_id;
 };
 
@@ -54,9 +54,9 @@ INLINE static void set_Til_iron(struct Til_params *mat,
   mat->b = 1.5f;
   mat->A = 1.28e12f;
   mat->B = 1.05e12f;
-  mat->E_0 = 9.5e10f;
-  mat->E_iv = 2.4e10f;
-  mat->E_cv = 8.67e10f;
+  mat->u_0 = 9.5e10f;
+  mat->u_iv = 2.4e10f;
+  mat->u_cv = 8.67e10f;
   mat->alpha = 5.0f;
   mat->beta = 5.0f;
   mat->eta_min = 0.0f;
@@ -70,9 +70,9 @@ INLINE static void set_Til_granite(struct Til_params *mat,
   mat->b = 1.3f;
   mat->A = 1.8e11f;
   mat->B = 1.8e11f;
-  mat->E_0 = 1.6e11f;
-  mat->E_iv = 3.5e10f;
-  mat->E_cv = 1.8e11f;
+  mat->u_0 = 1.6e11f;
+  mat->u_iv = 3.5e10f;
+  mat->u_cv = 1.8e11f;
   mat->alpha = 5.0f;
   mat->beta = 5.0f;
   mat->eta_min = 0.0f;
@@ -86,9 +86,9 @@ INLINE static void set_Til_water(struct Til_params *mat,
   mat->b = 0.15f;
   mat->A = 2.18e10f;
   mat->B = 1.325e11f;
-  mat->E_0 = 7.0e10f;
-  mat->E_iv = 4.19e9f;
-  mat->E_cv = 2.69e10f;
+  mat->u_0 = 7.0e10f;
+  mat->u_iv = 4.19e9f;
+  mat->u_cv = 2.69e10f;
   mat->alpha = 10.0f;
   mat->beta = 5.0f;
   mat->eta_min = 0.915f;
@@ -102,9 +102,9 @@ INLINE static void convert_units_Til(struct Til_params *mat,
   mat->rho_0 /= units_cgs_conversion_factor(us, UNIT_CONV_DENSITY);
   mat->A /= units_cgs_conversion_factor(us, UNIT_CONV_PRESSURE);
   mat->B /= units_cgs_conversion_factor(us, UNIT_CONV_PRESSURE);
-  mat->E_0 /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
-  mat->E_iv /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
-  mat->E_cv /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
+  mat->u_0 /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
+  mat->u_iv /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
+  mat->u_cv /= units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
   mat->P_min /= units_cgs_conversion_factor(us, UNIT_CONV_PRESSURE);
 }
 
@@ -156,35 +156,36 @@ INLINE static float Til_pressure_from_internal_energy(
     float density, float u, const struct Til_params *mat) {
 
   const float eta = density / mat->rho_0;
+  const float eta_sq = eta*eta;
   const float mu = eta - 1.f;
   const float nu = 1.f / eta - 1.f;
+  const float w = u / (mat->u_0 * eta_sq) + 1.f;
+  const float w_inv = 1.f / w_inv;
   float P_c, P_e, P;
 
   // Condensed or cold
   if (eta < mat->eta_min) {
     P_c = 0.f;
   } else {
-    P_c = (mat->a + mat->b / (u / (mat->E_0 * eta * eta) + 1.f)) * density * u +
-          mat->A * mu + mat->B * mu * mu;
+    P_c = (mat->a + mat->b*w_inv) * density * u + mat->A * mu + mat->B * mu*mu;
   }
   // Expanded and hot
   P_e = mat->a * density * u +
-        (mat->b * density * u / (u / (mat->E_0 * eta * eta) + 1.f) +
-         mat->A * mu * expf(-mat->beta * nu)) *
-            expf(-mat->alpha * nu * nu);
+        (mat->b * density * u * w_inv +
+         mat->A * mu * expf(-mat->beta * nu)) * expf(-mat->alpha * nu*nu);
 
   // Condensed or cold state
-  if ((1.f < eta) || (u < mat->E_iv)) {
+  if ((1.f < eta) || (u < mat->u_iv)) {
     P = P_c;
   }
   // Expanded and hot state
-  else if ((eta < 1.f) && (mat->E_cv < u)) {
+  else if ((eta < 1.f) && (mat->u_cv < u)) {
     P = P_e;
   }
   // Hybrid state
   else {
-    P = ((u - mat->E_iv) * P_e + (mat->E_cv - u) * P_c) /
-        (mat->E_cv - mat->E_iv);
+    P = ((u - mat->u_iv) * P_e + (mat->u_cv - u) * P_c) /
+        (mat->u_cv - mat->u_iv);
   }
 
   // Minimum pressure
@@ -208,65 +209,53 @@ INLINE static float Til_internal_energy_from_pressure(
 INLINE static float Til_soundspeed_from_internal_energy(
     float density, float u, const struct Til_params *mat) {
 
-  //    const float eta = density / mat->rho_0;
-  //    const float mu = eta - 1.f;
-  //    const float nu = 1.f/eta - 1.f;
-  //    float P_c, P_e, P, c_c, c_e, c;
-  //
-  //    // Condensed or cold
-  //    if (eta < mat->eta_min) {
-  //        P_c = 0.f;
-  //    }
-  //    else {
-  //        P_c = (mat->a + mat->b / (u / (mat->E_0 * eta*eta) + 1.f)) * density
-  //        * u
-  //            + mat->A * mu + mat->B * mu*mu;
-  //    }
-  //    c_c = mat->a*u + mat->b*u / ((u / (mat->E_0*eta*eta)+1.f) *
-  //        (u / (mat->E_0*eta*eta)+1.f)) *
-  //        (3.f*(u / (mat->E_0*eta*eta)+1.f) - 2.f) +
-  //        (mat->A + 2.f*mat->B*mu) / mat->rho_0  +  P_c / (rho*rho) *
-  //        (mat->a*rho + mat->b*rho / ((u / (mat->E_0*eta*eta)+1.f) *
-  //        (u / (mat->E_0*eta*eta)+1.f)));
-  //
-  //    c_c = max(c_c, mat->A / mat->rho_0);
-  //
-  //    // Expanded and hot
-  //    P_e = mat->a*density*u + (
-  //        mat->b * density * u / (u / (mat->E_0 * eta*eta) + 1.f)
-  //        + mat->A*mu * expf(-mat->beta * nu)
-  //        ) * expf(-mat->alpha * nu*nu);
-  //
-  //    c_e = (mat->a + mat->b / (u / (mat->E_0*eta*eta)+1.f) *
-  //        expf(-mat->beta*((1.f - eta)/eta)*((1.f - eta)/eta))
-  //        + 1.f)*P_e/rho + mat->A/mat->rho_0
-  //        *expf(-(mat->alpha*((1.f - eta)/eta)+mat->beta *
-  //        ((1.f - eta)/eta)*((1.f - eta)/eta)))*(1.f+mu/(eta*eta)
-  //        *(mat->alpha+2.f*mat->beta*((1.f - eta)/eta)-eta)) +
-  //        mat->b*rho*u/((u / (mat->E_0*eta*eta)+1.f)*
-  //        (u / (mat->E_0*eta*eta)+1.f)*eta*eta)*
-  //        expf(-mat->beta*((1.f - eta)/eta)*((1.f - eta)/eta))*
-  //        (2.f*mat->beta*((1.f - eta)/eta)*(u / (mat->E_0*eta*eta)+1.f) /
-  //         mat->rho_0 + 1.f/(mat->E_0*rho)*(2.f*u-P_e/rho));
-  //
-  //    // Condensed or cold state
-  //    if ((1.f < eta) || (u < mat->E_iv)) {
-  //        c = c_c;
-  //    }
-  //    // Expanded and hot state
-  //    else if ((eta < 1.f) && (mat->E_cv < u)) {
-  //        c = c_e;
-  //    }
-  //    // Hybrid state
-  //    else {
-  //		c = ((u - mat->E_iv)*c_e + (mat->E_cv - u)*c_c) /
-  //            (mat->E_cv - mat->E_iv);
-  //
-  //        c = max(c_c, mat->A / mat->rho0);
-  //    }
-  float c = sqrtf(mat->A / mat->rho_0);
+  const float eta = density / mat->rho_0;
+  const float rho_inv = 1.f / density;
+  const float eta_sq = eta*eta;
+  const float mu = eta - 1.f;
+  const float nu = 1.f / eta - 1.f;
+  const float w = u / (mat->u_0 * eta_sq) + 1.f;
+  const float w_inv = 1.f / w_inv;
+  const float w_inv_sq = w_inv*w_inv;
+  float P_c, P_e, P, c_sq_c, c_sq_e, c_sq;
 
-  return c;
+  // Condensed or cold
+  if (eta < mat->eta_min) {
+      P_c = 0.f;
+  }
+  else {
+    P_c = (mat->a + mat->b*w_inv) * density * u + mat->A * mu + mat->B * mu*mu;
+  }
+  c_sq_c = (1.f - mat->a - mat->b * w_inv) * P_c * rho_inv
+           + mat->b * (w - 1.f) * w_inv_sq * (2.f*u + P_c * rho_inv)
+           + (mat->A + mat->B * (eta_sq - 1.f)) * rho_inv;
+
+  c_sq_c = max(c_sq_c, mat->A / mat->rho_0);
+
+  // Expanded and hot
+  P_e = mat->a * density * u +
+        (mat->b * density * u * w_inv +
+         mat->A * mu * expf(-mat->beta * nu)) * expf(-mat->alpha * nu*nu);
+
+  c_sq_e = 1;///###wilo
+
+  // Condensed or cold state
+  if ((1.f < eta) || (u < mat->u_iv)) {
+      c_sq = c_sq_c;
+  }
+  // Expanded and hot state
+  else if ((eta < 1.f) && (mat->u_cv < u)) {
+      c_sq = c_sq_e;
+  }
+  // Hybrid state
+  else {
+    c_sq = ((u - mat->u_iv)*c_sq_e + (mat->u_cv - u)*c_sq_c) /
+          (mat->u_cv - mat->u_iv);
+
+    c_sq = max(c_sq_c, mat->A / mat->rho_0);
+  }
+
+  return sqrtf(c_sq);
 }
 
 // gas_soundspeed_from_pressure
