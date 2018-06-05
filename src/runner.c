63560cee6dad54d436efd983f4d23a6faacaf7f5
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-06-05 15:37:52 +0100

    Remove duplicate no neighbour flag setting

--- src/runner.c
+++ src/runner.c
@@ -720,9 +720,6 @@ void runner_do_ghost(struct runner *r, struct cell *c, int timer) {
           /* Double h and try again */
           h_new = 2.f * h_old;
 
-          /* Flag that there were no neighbours */
-          has_no_neighbours = 1;
-
         } else {
 
           /* Finish the density calculation */
