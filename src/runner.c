88ae8dee8301bc4c3f5226062b3a5a311daa5fa1
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-06-20 13:55:54 +0100

    Added include flags, minor function changes, for new scheme.

--- src/runner.c
+++ src/runner.c
@@ -885,8 +885,8 @@ void runner_do_extra_ghost(struct runner *r, struct cell *c, int timer) {
 
       if (part_is_active(p, e)) {
 
-        /* Finish the gradient calculation */
-        hydro_end_gradient(p);
+        /* Get ready for a force calculation */
+        hydro_end_gradient(p, e);
 
         /* As of here, particle force variables will be set. */
 
@@ -1120,6 +1120,7 @@ void runner_do_ghost(struct runner *r, struct cell *c, int timer) {
           }
         }
 
+(??)/* We now have a particle whose smoothing length has converged */
           /* We now have a particle whose smoothing length has converged */
 
 #ifdef EXTRA_HYDRO_LOOP
