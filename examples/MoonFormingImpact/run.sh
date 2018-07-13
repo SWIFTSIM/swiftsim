b4799e7d61a401ee9b0849ec276cd9b7c6d9ffee
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-07-13 14:34:12 +0100

    Remove for now the outdated planetary examples

--- examples/MoonFormingImpact/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -f snapshots/moon_forming_impact_??????.hdf5
-rm -f timesteps_*.txt
-
-../swift -G -s -t 8 moon_forming_impact.yml
--- examples/UranusImpact/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -f snapshots/uranus_impact_??????.hdf5
-rm -f timesteps_*.txt
-
-../swift -G -s -t 8 uranus_impact.yml
