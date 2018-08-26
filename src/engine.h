5cd6c6a4cac493168be29c74ec06acbe3d656669
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-08-26 13:39:23 +0100

    Add option to label snapshots by the time, replacing label_delta and label_first which fail with output_list and had the same aim anyway

--- src/engine.h
+++ src/engine.h
@@ -223,8 +223,7 @@ struct engine {
 
   char snapshot_base_name[PARSER_MAX_LINE_SIZE];
   int snapshot_compression;
-  int snapshot_label_first;
-  int snapshot_label_delta;
+  int snapshot_label_time_on;
   struct unit_system *snapshot_units;
   int snapshot_output_count;
 
