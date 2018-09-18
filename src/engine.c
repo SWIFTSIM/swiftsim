55361bb69bd0045de1c55598ca711f74f70d3000
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-09-18 15:29:25 +0100

    Formatting

--- src/engine.c
+++ src/engine.c
@@ -5040,8 +5040,8 @@ void engine_check_for_dumps(struct engine *e) {
     /* Perform structure finding? */
     if (run_stf) {
 
-    // MATTHIEU: Add a drift_all here. And check the order with the order i/o
-    // options.
+      // MATTHIEU: Add a drift_all here. And check the order with the order i/o
+      // options.
 
 #ifdef HAVE_VELOCIRAPTOR
       velociraptor_init(e);
