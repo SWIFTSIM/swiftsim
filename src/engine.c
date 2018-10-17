95fffaca02c966e50f1ca065cec29f8a2f8df59d
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2018-10-17 14:39:21 +0200

    Applied code formatting script.

--- src/engine.c
+++ src/engine.c
@@ -3356,8 +3356,8 @@ void engine_link_stars_tasks_mapper(void *map_data, int num_elements,
       /* Now, build all the dependencies for the stars */
       engine_make_stars_loops_dependencies(sched, t, t->ci);
       if (t->ci == t->ci->super)
-	scheduler_addunlock(sched, t->ci->super->stars.ghost_out,
-			    t->ci->super->end_force);
+        scheduler_addunlock(sched, t->ci->super->stars.ghost_out,
+                            t->ci->super->end_force);
     }
 
     /* Otherwise, pair interaction? */
