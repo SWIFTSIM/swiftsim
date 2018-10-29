44f7900be46a9996512f18c709b482c099f6995a
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-10-29 14:24:04 +0000

    Brought back the Hubble flow term in dvdr for the Pressure-X schemes in their AV

--- src/hydro/PressureEnergyMorrisMonaghanAV/hydro_iact.h
+++ src/hydro/PressureEnergyMorrisMonaghanAV/hydro_iact.h
@@ -288,7 +288,7 @@ __attribute__((always_inline)) INLINE static void runner_iact_force(
                               wj_dr * dvdr * r_inv;
 
   /* Viscosity term */
-  const float visc_du_term = 0.5f * visc_acc_term * dvdr;
+  const float visc_du_term = 0.5f * visc_acc_term * dvdr_Hubble;
 
   /* Assemble the energy equation term */
   const float du_dt_i = sph_du_term_i + visc_du_term;
@@ -410,7 +410,7 @@ __attribute__((always_inline)) INLINE static void runner_iact_nonsym_force(
                               wi_dr * dvdr * r_inv;
 
   /* Viscosity term */
-  const float visc_du_term = 0.5f * visc_acc_term * dvdr;
+  const float visc_du_term = 0.5f * visc_acc_term * dvdr_Hubble;
 
   /* Assemble the energy equation term */
   const float du_dt_i = sph_du_term_i + visc_du_term;
