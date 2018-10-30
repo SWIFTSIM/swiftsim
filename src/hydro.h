877407bee733ef1cbc0a86df6ba1aea2552c8962
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-10-30 12:04:25 +0000

    Fixed mis-spelling

--- src/hydro.h
+++ src/hydro.h
@@ -75,7 +75,7 @@
 #elif defined(ANARCHY_PU_SPH)
 #include "./hydro/AnarchyPU/hydro.h"
 #include "./hydro/AnarchyPU/hydro_iact.h"
-#define SPH_IMPLENETATION "ANARCHY (Pressure-Energy) SPH (Dalla Vecchia+ in prep)"
+#define SPH_IMPLEMENTATION "ANARCHY (Pressure-Energy) SPH (Dalla Vecchia+ in prep)"
 #else
 #error "Invalid choice of SPH variant"
 #endif
