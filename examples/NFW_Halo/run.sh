ca27046aeb16348e244e4d7a5be62144dc0c79e0
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-05 09:30:48 +0100

    Rename the Hernquist_Halo to Hernquist_circularorbit to be clearer

--- examples/NFW_Halo/run.sh
+++ examples/NFW_Halo/run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+if [ ! -i test_nfw.hdf5 ]
+    if command -v python3 &>/dev/null; then
+        python3 makeIC.py
+    else 
+        python makeIC.py
+    fi
+fi
+
 # self gravity G, external potential g, hydro s, threads t and high verbosity v
 ../swift -g -t 6 test.yml 2>&1 | tee output.log
 
 
+
+if command -v python3 &>/dev/null; then
+    python3 makePlots.py
+else 
+    python makePlots.py
+fi
