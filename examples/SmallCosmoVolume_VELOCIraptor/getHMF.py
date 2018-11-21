7662b6c314b2edc9dc2803dbb79f462b2c6dc4c0
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-21 16:35:31 +0100

    Format python using black and update .gitignore

--- examples/SmallCosmoVolume_VELOCIraptor/getHMF.py
+++ examples/SmallCosmoVolume_VELOCIraptor/getHMF.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
+###############################################################################
+# This file is part of SWIFT.
+# Copyright (c) 2018 Folkert Nobels (nobels@strw.leidenuniv.nl)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
 import numpy as np
 from hmf import MassFunction
 import hmf
 from astropy.cosmology import FlatLambdaCDM
 
 
-def getHMF(H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=1e15):
-    ''' Fast function to call the HMF from hmf, this function only has 
+def getHMF(H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10, Mmax=1e15):
+    """ Fast function to call the HMF from hmf, this function only has 
         6 variables and will return the dn/d(log10 M) and M array.
         H0: Hubble constant
         Om0: Matter density
@@ -14,13 +32,16 @@ def getHMF(H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=1e15):
         Tcmb0: CMB temperature at z=0
         Mmin: minimum mass (solar masses)
         Mmax: Maximum mass (solar masses) 
-    '''
-    new_model = FlatLambdaCDM(H0 = H0, Om0=Om0, Ob0 = Ob0, Tcmb0=Tcmb0)
-    hmff = MassFunction(cosmo_model=new_model, Mmax = np.log10(Mmax), Mmin=np.log10(Mmin),hmf_model='ST')
+    """
+    new_model = FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0, Tcmb0=Tcmb0)
+    hmff = MassFunction(
+        cosmo_model=new_model, Mmax=np.log10(Mmax), Mmin=np.log10(Mmin), hmf_model="ST"
+    )
     return hmff.m, hmff.dndlog10m
 
-def getHMFz(z,H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=1e15):
-    ''' Fast function to call the HMF from hmf, this function only has 
+
+def getHMFz(z, H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10, Mmax=1e15):
+    """ Fast function to call the HMF from hmf, this function only has 
         7 variables and will return the dn/d(log10 M) and M array.
         z: redshift
         H0: Hubble constant
@@ -29,13 +50,20 @@ def getHMFz(z,H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=1e15):
         Tcmb0: CMB temperature at z=0
         Mmin: minimum mass (solar masses)
         Mmax: Maximum mass (solar masses) 
-    '''
-    new_model = FlatLambdaCDM(H0 = H0, Om0=Om0, Ob0 = Ob0, Tcmb0=Tcmb0)
-    hmff = MassFunction(cosmo_model=new_model, Mmax = np.log10(Mmax), Mmin=np.log10(Mmin),z=z,hmf_model='ST')
+    """
+    new_model = FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0, Tcmb0=Tcmb0)
+    hmff = MassFunction(
+        cosmo_model=new_model,
+        Mmax=np.log10(Mmax),
+        Mmin=np.log10(Mmin),
+        z=z,
+        hmf_model="ST",
+    )
     return hmff.m, hmff.dndlog10m
 
-def getHMFztinker(z,H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=1e15):
-    ''' Fast function to call the HMF from hmf, this function only has 
+
+def getHMFztinker(z, H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10, Mmax=1e15):
+    """ Fast function to call the HMF from hmf, this function only has 
         6 variables and will return the dn/d(log10 M) and M array.
         H0: Hubble constant
         Om0: Matter density
@@ -43,22 +71,11 @@ def getHMFztinker(z,H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10,Mmax=
         Tcmb0: CMB temperature at z=0
         Mmin: minimum mass (solar masses)
         Mmax: Maximum mass (solar masses) 
-    '''
-    new_model = FlatLambdaCDM(H0 = H0, Om0=Om0, Ob0 = Ob0, Tcmb0=Tcmb0)
-    hmff = MassFunction(cosmo_model=new_model, Mmax = np.log10(Mmax), Mmin=np.log10(Mmin),z=z)
+    """
+    new_model = FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0, Tcmb0=Tcmb0)
+    hmff = MassFunction(
+        cosmo_model=new_model, Mmax=np.log10(Mmax), Mmin=np.log10(Mmin), z=z
+    )
     return hmff.m, hmff.dndlog10m
 
-'''
-hmmf = MassFunction()
-hmmf.parameter_info()
-print('Statement 2:')
-print(hmmf.hmf_model)
-model_st = hmf.fitting_functions.ST([1,2])
-print(model_st)
 
-print(getHMFztinker(10))
-
-
-#print(getHMFztinker(1.0))
-
-'''
