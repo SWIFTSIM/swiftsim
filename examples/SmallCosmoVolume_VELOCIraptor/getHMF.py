b9ded64ea766dacc72890f2122586926dbdcdf6f
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-21 16:51:00 +0100

    Update python script

--- examples/SmallCosmoVolume_VELOCIraptor/getHMF.py
+++ examples/SmallCosmoVolume_VELOCIraptor/getHMF.py
@@ -23,22 +23,6 @@ import hmf
 from astropy.cosmology import FlatLambdaCDM
 
 
-def getHMF(H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10, Mmax=1e15):
-    """ Fast function to call the HMF from hmf, this function only has 
-        6 variables and will return the dn/d(log10 M) and M array.
-        H0: Hubble constant
-        Om0: Matter density
-        Ob0: Baryon density
-        Tcmb0: CMB temperature at z=0
-        Mmin: minimum mass (solar masses)
-        Mmax: Maximum mass (solar masses) 
-    """
-    new_model = FlatLambdaCDM(H0=H0, Om0=Om0, Ob0=Ob0, Tcmb0=Tcmb0)
-    hmff = MassFunction(
-        cosmo_model=new_model, Mmax=np.log10(Mmax), Mmin=np.log10(Mmin), hmf_model="ST"
-    )
-    return hmff.m, hmff.dndlog10m
-
 
 def getHMFz(z, H0=70.3, Om0=0.276, Ob0=0.0455, Tcmb0=2.725, Mmin=1e10, Mmax=1e15):
     """ Fast function to call the HMF from hmf, this function only has 
