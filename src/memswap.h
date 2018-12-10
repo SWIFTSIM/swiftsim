3a937a0ab2ba22d08ffd1b58d0cf9d20f928a98a
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2018-12-10 16:35:33 +0000

    Modern C approach to memswap after we found issues on ARM architectures with some compilers with the old version

--- src/memswap.h
+++ src/memswap.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of SWIFT.
  * Copyright (c) 2016 Pedro Gonnet (pedro.gonnet@durham.ac.uk)
- *
+ *               2018 STFC (author email aidan.chalk@stfc.ac.uk)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
