0616aecb9a1661ded8b1aad9e6439c4f3b40ec8e
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-12-19 13:45:54 +0000

    Tidy up unnecessary inverse term

--- src/hydro/Planetary/hydro.h
+++ src/hydro/Planetary/hydro.h
@@ -524,9 +524,7 @@ __attribute__((always_inline)) INLINE static void hydro_prepare_force(
   if (p->h == hydro_props->h_max) {
     rho_dh = 0.f;
   }
-  const float grad_h_term_inv =
-      1.f + hydro_dimension_inv * p->h * rho_dh * rho_inv;
-  grad_h_term = 1.f / grad_h_term_inv;
+  grad_h_term = 1.f / (1.f + hydro_dimension_inv * p->h * rho_dh * rho_inv);
 
   /* Compute the Balsara switch */
 #ifdef PLANETARY_SPH_NO_BALSARA
