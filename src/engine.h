c8373c44bdaf7edf828f8d82da7ed52dcd16aa0b
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2018-12-28 17:24:36 +0000

    Fix the variables used with atomic_inc to be the correct atomic types

--- src/engine.h
+++ src/engine.h
@@ -308,7 +308,7 @@ struct engine {
   int restarting;
 
   /* Force the engine to rebuild? */
-  int forcerebuild;
+  atomic_int forcerebuild;
 
   /* Force the engine to repartition ? */
   int forcerepart;
@@ -323,7 +323,8 @@ struct engine {
 
   /* Linked list for cell-task association. */
   struct link *links;
-  size_t nr_links, size_links;
+  atomic_size_t nr_links;
+  size_t size_links;
 
   /* Average number of tasks per cell. Used to estimate the sizes
    * of the various task arrays. */
