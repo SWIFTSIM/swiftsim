1bca87864202f2620c3733eb008edd6205ebd655
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-02-22 15:06:54 +0000

    EAGLE SF -> COLIBRE SF

--- src/star_formation/COLIBRE/star_formation_struct.h
+++ src/star_formation/COLIBRE/star_formation_struct.h
@@ -16,26 +16,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#ifndef SWIFT_STAR_FORMATION_IO_H
-#define SWIFT_STAR_FORMATION_IO_H
+#ifndef SWIFT_COLIBRE_STAR_FORMATION_STRUCT_H
+#define SWIFT_COLIBRE_STAR_FORMATION_STRUCT_H
 
 /**
- * @file src/star_formation_io.h
- * @brief Branches between the i/o routines for the SF code.
+ * @brief Star-formation-related properties stored in the extended particle
+ * data.
  */
+struct star_formation_xpart_data {
 
-/* Config parameters. */
-#include "../config.h"
+  /*! Star formation rate */
+  float SFR;
+};
 
-/* Import the right cooling definition */
-#if defined(STAR_FORMATION_NONE)
-#include "./star_formation/none/star_formation_io.h"
-#elif defined(STAR_FORMATION_EAGLE)
-#include "./star_formation/EAGLE/star_formation_io.h"
-#elif defined(STAR_FORMATION_COLIBRE)
-#include "./star_formation/COLIBRE/star_formation_io.h"
-#else
-#error "Invalid choice of star formation model."
-#endif
-
-#endif /* SWIFT_STAR_FORMATION_IO_H */
+#endif /* SWIFT_COLIBRE_STAR_FORMATION_STRUCT_H */
