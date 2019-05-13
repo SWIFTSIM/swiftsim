b3130e66b6ae629b2a2e05294d1ea12e3e397270
Sylvia Ploeckinger <ploeckinger@strw.leidenuniv.nl>
2019-05-13 14:14:00 +0100

    Rebasing and resolving lots of conflicts

--- src/cooling/COLIBRE/cooling.c
+++ src/cooling/COLIBRE/cooling.c
@@ -45,6 +45,7 @@
 #include "parser.h"
 #include "part.h"
 #include "physical_constants.h"
+#include "space.h"
 #include "units.h"
 
 /* Maximum number of iterations for
@@ -62,11 +63,14 @@ static const double bracket_factor = 1.5; /* sqrt(1.1) */
  * given time-step or redshift. Predominantly used to read cooling tables
  * above and below the current redshift, if not already read in.
  *
+ * Also calls the additional H reionisation energy injection if need be.
+ *
  * @param cosmo The current cosmological model.
  * @param cooling The #cooling_function_data used in the run.
+ * @param s The space data, including a pointer to array of particles
  */
 void cooling_update(const struct cosmology *cosmo,
-                    struct cooling_function_data *cooling) {
+                    struct cooling_function_data *cooling, struct space *s) {
 
   /* Current redshift */
   const float redshift = cosmo->z;
@@ -365,8 +369,10 @@ void cooling_cool_part(const struct phys_const *phys_const,
      re-ionization as this needs to be added on no matter what */
 
   /* Get helium and hydrogen reheating term */
-  const double Helium_reion_heat_cgs = eagle_helium_reionization_extraheat(
-      cooling->z_index, delta_redshift, cooling);
+  const double Helium_reion_heat_cgs =
+      eagle_helium_reionization_extraheat(cosmo->z, delta_redshift, cooling);
+
+  /* Convert this into a rate */
   const double Lambda_He_reion_cgs =
       Helium_reion_heat_cgs / (dt_cgs * ratefact_cgs);
 
@@ -568,6 +574,39 @@ __attribute__((always_inline)) INLINE float cooling_get_radiated_energy(
 }
 
 /**
+ * @brief Inject a fixed amount of energy to each particle in the simulation
+ * to mimic Hydrogen reionization.
+ *
+ * @param cooling The properties of the cooling model.
+ * @param cosmo The cosmological model.
+ * @param s The #space containing the particles.
+ */
+void cooling_Hydrogen_reionization(const struct cooling_function_data *cooling,
+                                   const struct cosmology *cosmo,
+                                   struct space *s) {
+
+  struct part *parts = s->parts;
+  struct xpart *xparts = s->xparts;
+
+  /* Energy to inject in internal units */
+  const float extra_heat =
+      cooling->H_reion_heat_cgs * cooling->internal_energy_from_cgs;
+
+  /* Loop through particles and set new heat */
+  for (size_t i = 0; i < s->nr_parts; i++) {
+
+    struct part *p = &parts[i];
+    struct xpart *xp = &xparts[i];
+
+    const float old_u = hydro_get_physical_internal_energy(p, xp, cosmo);
+    const float new_u = old_u + extra_heat;
+
+    hydro_set_physical_internal_energy(p, xp, cosmo, new_u);
+    hydro_set_drifted_physical_internal_energy(p, cosmo, new_u);
+  }
+}
+
+/**
  * @brief Initialises properties stored in the cooling_function_data struct
  *
  * @param parameter_file The parsed parameter file
@@ -581,8 +620,15 @@ void cooling_init_backend(struct swift_params *parameter_file,
                           struct cooling_function_data *cooling) {
 
   /* read some parameters */
+
+  /* Despite the names, the values of H_reion_heat_cgs and He_reion_heat_cgs
+   * that are read in are actually in units of electron volts per proton mass.
+   * We later convert to units just below */
+
   parser_get_param_string(parameter_file, "COLIBRECooling:dir_name",
                           cooling->cooling_table_path);
+
+  cooling->H_reion_done = 0;
   cooling->H_reion_z =
       parser_get_param_float(parameter_file, "COLIBRECooling:H_reion_z");
   cooling->H_reion_heat_cgs =
@@ -600,10 +646,19 @@ void cooling_init_backend(struct swift_params *parameter_file,
   cooling->S_over_Si_ratio_in_solar = parser_get_opt_param_float(
       parameter_file, "COLIBRECooling:S_over_Si_in_solar", 1.f);
 
-  /* Convert to cgs (units used internally by the cooling routines) */
+  /* Convert H_reion_heat_cgs and He_reion_heat_cgs to cgs
+   * (units used internally by the cooling routines). This is done by
+   * multiplying by 'eV/m_H' in internal units, then converting to cgs units.
+   * Note that the dimensions of these quantities are energy/mass = velocity^2
+   */
+
+  cooling->H_reion_heat_cgs *=
+      phys_const->const_electron_volt / phys_const->const_proton_mass *
+      units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
+
   cooling->He_reion_heat_cgs *=
-      phys_const->const_electron_volt *
-      units_cgs_conversion_factor(us, UNIT_CONV_ENERGY);
+      phys_const->const_electron_volt / phys_const->const_proton_mass *
+      units_cgs_conversion_factor(us, UNIT_CONV_ENERGY_PER_UNIT_MASS);
 
   read_cooling_header(cooling);
   read_cooling_tables(cooling);
@@ -767,3 +822,4 @@ void cooling_struct_restore(struct cooling_function_data *cooling, FILE *stream,
 
   cooling_restore_tables(cooling, cosmo);
 }
+
