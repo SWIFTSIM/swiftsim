37085167bf9c5e0357dc627fca8ff491688c3c10
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-07-08 10:12:45 +0200

    Add broken power law to the DTDs

---
+++ src/feedback/DTD/broken_power_law/dtd_struct.h
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2019 Folkert Nobels (nobels@strw.leidenuniv.nl)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+/**
+ * @brief SNIa struct for DTD properties
+ */
+struct dtd {
+
+  /*! Efficiency of the SNIa model */
+  double SNIa_efficiency;
+
+  /*! Power short time power law */
+  double power_short_time;
+
+  /*! Power long time power law */
+  double power_long_time;
+
+  /*! Power law break time */
+  double break_time_Gyr;
+
+  /*! Normalization time scale */
+  double normalization_timescale_Gyr;
+
+  /*! Delay time */
+  double delay_time_Gyr;
+
+  /*! DTD normalization */
+  double norm;
+};
