b96e20307ad586a27f333091124fd7cb7744cbe3
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-08-22 10:14:18 +0200

    Update run.sh and add config file

--- examples/SubgridTests/StochasticSNIa/run.sh
+++ examples/SubgridTests/StochasticSNIa/run.sh
@@ -18,22 +18,22 @@ then
     ./getYieldTable.sh
 fi
 
-../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.002 2>&1 | tee output.log
+../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.002 2>&1 | tee output.log
 
 python3 plotSolution.py nu0.002
 
-../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.001 2>&1 | tee output.log
+../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.001 2>&1 | tee output.log
 
 python3 plotSolution.py nu0.001
 
-../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.004 2>&1 | tee output.log
+../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_efficiency_p_Msun:0.004 2>&1 | tee output.log
 
 python3 plotSolution.py nu0.004
 
-../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml -P SNIaDTD:normalization_timescale_Gyr:6.0 2>&1 | tee output.log
+../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --cooling --hydro isolated_galaxy.yml -P SNIaDTD:normalization_timescale_Gyr:6.0 2>&1 | tee output.log
 
 python3 plotSolution.py nu0.002_norm6
 
-../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_delay_time_Gyr:0.08 2>&1 | tee output.log
+../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --cooling --hydro isolated_galaxy.yml -P SNIaDTD:SNIa_delay_time_Gyr:0.08 2>&1 | tee output.log
 
 python3 plotSolution.py nu0.002_t_init
