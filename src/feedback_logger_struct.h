4e72caba27cbc78205c9617737dba18d6419da49
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-09-02 15:24:19 +0200

    Add the feedback logger struct to the root directory

---
+++ src/feedback_logger_struct.h
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Coypright (c) 2019 Folkert Nobels (nobels@strw.leidenuniv.nl)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#ifndef SWIFT_FEEDBACK_LOGGER_STRUCT_H
+#define SWIFT_FEEDBACK_LOGGER_STRUCT_H
+
+/* Config parameters. */
+#include "../config.h"
+
+/* Select the correct feedback model */
+#if defined(FEEDBACK_NONE)
+#include "./feedback/none/feedback_logger_struct.h"
+#elif defined(FEEDBACK_EAGLE)
+#include "./feedback/EAGLE/feedback_logger_struct.h"
+#elif defined(FEEDBACK_COLIBRE)
+#include "./feedback/COLIBRE/feedback_logger_struct.h"
+#else
+#error "Invalid choice of feedback model"
+#endif
+
+#endif
