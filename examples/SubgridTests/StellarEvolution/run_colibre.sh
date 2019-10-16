af0b2869a85fcdcb316885ebcf84f909496397dd
James <j.w.trayford@durham.ac.uk>
2019-10-16 12:49:13 +0100

    modify run script for COLIBRE stellar evolution test w/dust

---
+++ examples/SubgridTests/StellarEvolution/run_colibre.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# # Generate the initial conditions if they are not present.
+# if [ ! -e glassCube_32.hdf5 ]
+# then
+#     echo "Fetching initial glass file for the Supernovae feedback example..."
+#     ./getGlass.sh
+# fi
+# if [ ! -e stellar_evolution.hdf5 ]
+# then
+#     echo "Generating initial conditions for the 3D stellar evolution example..."
+#     python makeIC.py
+# fi
+
+# # Get the Yield tables
+# if [ ! -e yieldtables ]
+# then
+#     echo "Fetching Yield tables..."
+#     ./getEagleYieldTable.sh
+# fi
+
+# # Get the solutions
+# if [ ! -e StellarEvolutionSolution ]
+# then
+#     echo "Fetching solutions ..."
+#     ./getSolutions.sh
+# fi
+
+../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=16 stellar_evolution_dust.yml -P COLIBREChemistry:init_abundance_metal:0.08 2>&1 | tee output_0p08.log
+
+python dustevo.py
+
+../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=16 stellar_evolution_dust.yml -P COLIBREChemistry:init_abundance_metal:0.04 2>&1 | tee output_0p04.log
+
+python dustevo.py
+
+../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=16 stellar_evolution_dust.yml -P COLIBREChemistry:init_abundance_metal:0.01 2>&1 | tee output_0p01.log
+
+python dustevo.py
+
+../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=16 stellar_evolution_dust.yml -P COLIBREChemistry:init_abundance_metal:0.001 2>&1 | tee output_0p001.log
+
+python dustevo.py
+
+../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=16 stellar_evolution_dust.yml -P COLIBREChemistry:init_abundance_metal:0.0001 2>&1 | tee output_0p0001.log
+
+python dustevo.py
+# python plot_box_evolution.py
