44660f6c37027e11888761e914f134cd72e4c4e4
Sylvia Ploeckinger <ploeckinger@strw.leidenuniv.nl>
2019-11-27 10:32:31 +0000

    Only allocate/free the `buf` if we are going to print the CPU mask.

--- src/engine.c
+++ src/engine.c
@@ -3594,15 +3594,18 @@ void engine_config(int restart, int fof, struct engine *e,
   if (nr_cores > CPU_SETSIZE) /* Unlikely, except on e.g. SGI UV. */
     error("must allocate dynamic cpu_set_t (too many cores per node)");
 
-  char *buf = (char *)malloc((nr_cores + 1) * sizeof(char));
-  buf[nr_cores] = '\0';
-  for (int j = 0; j < nr_cores; ++j) {
-    /* Reversed bit order from convention, but same as e.g. Intel MPI's
-     * I_MPI_PIN_DOMAIN explicit mask: left-to-right, LSB-to-MSB. */
-    buf[j] = CPU_ISSET(j, entry_affinity) ? '1' : '0';
-  }
 
-  if (verbose && with_aff) message("Affinity at entry: %s", buf);
+  if (verbose && with_aff) {
+      char *buf = (char *)malloc((nr_cores + 1) * sizeof(char));
+      buf[nr_cores] = '\0';
+      for (int j = 0; j < nr_cores; ++j) {
+        /* Reversed bit order from convention, but same as e.g. Intel MPI's
+         * I_MPI_PIN_DOMAIN explicit mask: left-to-right, LSB-to-MSB. */
+        buf[j] = CPU_ISSET(j, entry_affinity) ? '1' : '0';
+      }
+      message("Affinity at entry: %s", buf);
+      free(buf);
+  }
 
   int *cpuid = NULL;
   cpu_set_t cpuset;
@@ -4208,7 +4211,6 @@ void engine_config(int restart, int fof, struct engine *e,
   if (with_aff) {
     free(cpuid);
   }
-  free(buf);
 #endif
 
   /* Wait for the runner threads to be in place. */
