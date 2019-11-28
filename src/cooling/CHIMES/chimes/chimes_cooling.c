01f182697140c24b8f782ee5f519d3bbaf7be591
Alexander James Richings <a.j.richings@northwestern.edu>
2019-11-28 17:22:13 +0000

    Update to the CHIMES-HYBRID cooling mode.
    We only add the metal cooling from the COLIBRE cooling tables if the metallicity is above the minimum (non-primordial) metallicity bin in the COLIBRE tables. This allows us to skip interpolating these tables for primordial particles.

--- src/cooling/CHIMES/chimes/chimes_cooling.c
+++ src/cooling/CHIMES/chimes/chimes_cooling.c
@@ -330,7 +330,10 @@ ChimesFloat calculate_total_cooling_rate(struct gasVariables *myGasVars, struct 
    ** SWIFT-specific code **
    *************************/ 
   if (myGlobalVars->hybrid_cooling_mode == 1) 
-    total_heating += colibre_metal_cooling_rate_temperature(log10(myGasVars->temperature), myGlobalVars->redshift, myGasVars->nH_tot, myGasVars->metallicity * 0.0129, myGasVars->abundances[sp_elec], myGasVars->abundance_ratio, myGlobalVars->colibre_table); 
+    {
+      if (myGasVars->metallicity > pow(10.0, myGlobalVars->colibre_table->Metallicity[1])) 
+	total_heating += colibre_metal_cooling_rate_temperature(log10(myGasVars->temperature), myGlobalVars->redshift, myGasVars->nH_tot, myGasVars->metallicity * 0.0129, myGasVars->abundances[sp_elec], myGasVars->abundance_ratio, myGlobalVars->colibre_table); 
+    }
   /** End SWIFT-specific **/  
 
   // Convert units to erg/cm3/s 
