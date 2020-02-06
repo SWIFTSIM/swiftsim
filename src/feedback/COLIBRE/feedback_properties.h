32f55dc2d2d0a7d42361b3140613b6954c57a5c1
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-02-06 17:48:44 +0100

    Compute the jump in energy to apply in HII region from the properties given in the COLIBRE cooling section of the parameter file

--- src/feedback/COLIBRE/feedback_properties.h
+++ src/feedback/COLIBRE/feedback_properties.h
@@ -272,6 +272,9 @@ struct feedback_props {
   /* Recombination coefficient in cgs units [cm3 s-1]*/
   float alpha_caseb_recomb;
 
+  /*! Change in internal energy applied by HII region feedback */
+  float HII_delta_u;
+
   /* Number of age bins */
   int HII_nr_agebins;
 
