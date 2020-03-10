cef5f988ab5c5730726457a56a319e44044bae3f
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-03-10 17:09:55 +0100

    Store the last time a particle was hit by SNII/SNIa/AGN/kinetic COLIBRE feedback in the particles tracers.

--- src/runner_doiact_functions_stars.h
+++ src/runner_doiact_functions_stars.h
@@ -112,8 +112,9 @@ void DOSELF1_STARS(struct runner *r, struct cell *c, int timer) {
         runner_iact_nonsym_feedback_density(r2, dx, hi, hj, si, pj, NULL, cosmo,
                                             ti_current);
 #elif (FUNCTION_TASK_LOOP == TASK_LOOP_FEEDBACK)
-        runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, si, pj, xpj, cosmo,
-                                          ti_current, e->time, e->step);
+        runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, si, pj, xpj,
+                                          with_cosmology, cosmo, ti_current,
+                                          e->time, e->step);
 #endif
       }
     } /* loop over the parts in ci. */
@@ -221,8 +222,9 @@ void DO_NONSYM_PAIR1_STARS_NAIVE(struct runner *r, struct cell *restrict ci,
         runner_iact_nonsym_feedback_density(r2, dx, hi, hj, si, pj, NULL, cosmo,
                                             ti_current);
 #elif (FUNCTION_TASK_LOOP == TASK_LOOP_FEEDBACK)
-        runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, si, pj, xpj, cosmo,
-                                          ti_current, e->time, e->step);
+        runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, si, pj, xpj,
+                                          with_cosmology, cosmo, ti_current,
+                                          e->time, e->step);
 #endif
       }
     } /* loop over the parts in cj. */
@@ -393,8 +395,9 @@ void DO_SYM_PAIR1_STARS(struct runner *r, struct cell *ci, struct cell *cj,
           runner_iact_nonsym_feedback_density(r2, dx, hi, hj, spi, pj, NULL,
                                               cosmo, ti_current);
 #elif (FUNCTION_TASK_LOOP == TASK_LOOP_FEEDBACK)
-          runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, spi, pj, xpj, cosmo,
-                                            ti_current, e->time, e->step);
+          runner_iact_nonsym_feedback_apply(r2, dx, hi, hj, spi, pj, xpj,
+                                            with_cosmology, cosmo, ti_current,
+                                            e->time, e->step);
 #endif
         }
       } /* loop over the parts in cj. */
@@ -521,8 +524,9 @@ void DO_SYM_PAIR1_STARS(struct runner *r, struct cell *ci, struct cell *cj,
           runner_iact_nonsym_feedback_density(r2, dx, hj, hi, spj, pi, xpi,
                                               cosmo, ti_current);
 #elif (FUNCTION_TASK_LOOP == TASK_LOOP_FEEDBACK)
-          runner_iact_nonsym_feedback_apply(r2, dx, hj, hi, spj, pi, xpi, cosmo,
-                                            ti_current, e->time, e->step);
+          runner_iact_nonsym_feedback_apply(r2, dx, hj, hi, spj, pi, xpi,
+                                            with_cosmology, cosmo, ti_current,
+                                            e->time, e->step);
 #endif
         }
       } /* loop over the parts in ci. */
