9f483ca2c18ada33f18b026cb07925b41b9f8715
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-03-31 22:47:40 +0200

    Added 'const's in the new section.

--- src/cooling/COLIBRE/cooling_rates.h
+++ src/cooling/COLIBRE/cooling_rates.h
@@ -193,11 +193,11 @@ __attribute__((always_inline)) INLINE static float abundance_ratio_to_solar(
   const float logZZsol = log10f(ZZsol); */
 
   /* Get total metallicity [Z/Z_sun] from the particle data */
-  float Z_total =
+  const float Z_total =
       (float)chemistry_get_total_metal_mass_fraction_for_star_formation(p);
   float ZZsol = Z_total * cooling->Zsol_inv[0];
   if (ZZsol <= 0.0f) ZZsol = FLT_MIN;
-  float logZZsol = log10f(ZZsol);
+  const float logZZsol = log10f(ZZsol);
 
   /* All other elements (element_OA): scale with metallicity */
   ratio_solar[element_OA] = ZZsol;
