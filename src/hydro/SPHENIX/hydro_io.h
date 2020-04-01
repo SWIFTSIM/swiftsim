0c89e237cfeb5269ff603a3e94f3dbd5b087c232
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-04-01 20:40:16 +0100

    Added comments about hubble term included in div_v

--- src/hydro/SPHENIX/hydro_io.h
+++ src/hydro/SPHENIX/hydro_io.h
@@ -212,20 +212,23 @@ INLINE static void hydro_write_particles(const struct part* parts,
       convert_diffusion, "Diffusion coefficient (alpha_diff) of the particles");
 
   list[11] = io_make_output_field(
-      "LaplacianInternalEnergies", FLOAT, 1, UNIT_CONV_FREQUENCY_SQUARED, 0.f,
-      parts, diffusion.laplace_u,
+      "LaplacianInternalEnergies", FLOAT, 1, UNIT_CONV_FREQUENCY_SQUARED,
+      5.f - 3.f * hydro_gamma, parts, diffusion.laplace_u,
       "Laplacian (del squared) of the Internal Energy per "
       "unit mass of the particles");
 
   list[12] = io_make_output_field(
       "VelocityDivergences", FLOAT, 1, UNIT_CONV_FREQUENCY, 0.f, parts,
-      viscosity.div_v, "Local velocity divergence field around the particles");
+      viscosity.div_v,
+      "Local velocity divergence field around the particles. Provided without "
+      "cosmology, as this includes the Hubble flow.");
 
   list[13] = io_make_output_field(
       "VelocityDivergenceTimeDifferentials", FLOAT, 1,
       UNIT_CONV_FREQUENCY_SQUARED, 0.f, parts, viscosity.div_v_dt,
       "Time differential (over the previous step) of the "
-      "velocity divergence field around the particles");
+      "velocity divergence field around the particles. Again, provided without "
+      "cosmology as this includes a Hubble flow term.");
 }
 
 /**
