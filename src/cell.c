84b8a6480d7f195f4e5b8276b6d7ba56e862ee4c
loikki <loic.hausammann@protonmail.ch>
2020-04-03 11:52:14 +0200

    Logger: log particle leaving the box

--- src/cell.c
+++ src/cell.c
@@ -4687,6 +4687,14 @@ void cell_drift_part(struct cell *c, const struct engine *e, int force) {
            * by another thread before we do the deed. */
           if (!part_is_inhibited(p, e)) {
 
+#ifdef WITH_LOGGER
+            if (e->policy & engine_policy_logger) {
+              /* Log the particle a last time */
+              const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+              logger_log_part(e->logger, p, xp, e, /* log_all */ 1, flag);
+            }
+#endif
+
             /* One last action before death? */
             hydro_remove_part(p, xp);
 
@@ -4857,6 +4865,16 @@ void cell_drift_gpart(struct cell *c, const struct engine *e, int force) {
 
             /* Remove the particle entirely */
             if (gp->type == swift_type_dark_matter) {
+
+#ifdef WITH_LOGGER
+              if (e->policy & engine_policy_logger) {
+                /* Log the particle a last time */
+                const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+                logger_log_gpart(e->logger, gp, e, /* log_all */ 1, flag);
+              }
+#endif
+
+              /* Remove the particle */
               cell_remove_gpart(e, c, gp);
             }
           }
@@ -4997,6 +5015,14 @@ void cell_drift_spart(struct cell *c, const struct engine *e, int force) {
           /* Re-check that the particle has not been removed
            * by another thread before we do the deed. */
           if (!spart_is_inhibited(sp, e)) {
+
+#ifdef WITH_LOGGER
+            if (e->policy & engine_policy_logger) {
+              /* Log the particle a last time */
+              const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+              logger_log_spart(e->logger, sp, e, /* log_all */ 1, flag);
+            }
+#endif
 
             /* Remove the particle entirely */
             cell_remove_spart(e, c, sp);
@@ -5168,6 +5194,12 @@ void cell_drift_bpart(struct cell *c, const struct engine *e, int force) {
           /* Re-check that the particle has not been removed
            * by another thread before we do the deed. */
           if (!bpart_is_inhibited(bp, e)) {
+
+#ifdef WITH_LOGGER
+            if (e->policy & engine_policy_logger) {
+              error("The black holes are not implemented.");
+            }
+#endif
 
             /* Remove the particle entirely */
             cell_remove_bpart(e, c, bp);
