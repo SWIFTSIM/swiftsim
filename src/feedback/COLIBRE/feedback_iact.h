116f56786c7db8a3f3017a7dac5ee2bc7c3345f3
Camila Correa <correa@strw.leidenuniv.nl>
2020-04-23 16:44:32 +0200

    Adding to reset diffusion redshifts once they are used

--- src/feedback/COLIBRE/feedback_iact.h
+++ src/feedback/COLIBRE/feedback_iact.h
@@ -160,6 +160,7 @@ runner_iact_nonsym_feedback_apply(const float r2, const float *dx,
       pj->chemistry_data.metal_weighted_redshift * current_metal_mass_total +
       pj->chemistry_data.metal_diffused_redshift;
     pj->chemistry_data.metal_weighted_redshift /= new_metal_mass_total;
+    pj->chemistry_data.metal_diffused_redshift = 0.f;
   }
     
 
@@ -201,6 +202,7 @@ runner_iact_nonsym_feedback_apply(const float r2, const float *dx,
         pj->chemistry_data.iron_weighted_redshift * current_iron_from_SNIa_mass +
         pj->chemistry_data.iron_diffused_redshift;
       pj->chemistry_data.iron_weighted_redshift /= new_iron_from_SNIa_mass;
+      pj->chemistry_data.iron_diffused_redshift= 0.f;
   }
 
   /* Update mass from SNIa  */
