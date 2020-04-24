7ff5717823b0bde07f79dea27316404d9403ec4c
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2020-04-24 09:23:34 +0100

    Add the COLIBRE EoS parameters to the stellar evolution example

--- examples/SubgridTests/StellarEvolution/run.sh
+++ examples/SubgridTests/StellarEvolution/run.sh
@@ -9,7 +9,7 @@ fi
 if [ ! -e stellar_evolution.hdf5 ]
 then
     echo "Generating initial conditions for the 3D stellar evolution example..."
-    python makeIC.py
+    python3 makeIC.py
 fi
 
 # Get the Yield tables
@@ -28,20 +28,20 @@ fi
 
 ../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=4 stellar_evolution.yml -P EAGLEChemistry:init_abundance_metal:0.08 -P EAGLEChemistry:init_abundance_Hydrogen:0.71 -P EAGLEChemistry:init_abundance_Helium:0.21 -P COLIBREChemistry:init_abundance_metal:0.08 2>&1 | tee output_0p08.log
 
-python plot_box_evolution.py
+python3 plot_box_evolution.py
 
 ../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=4 stellar_evolution.yml -P EAGLEChemistry:init_abundance_metal:0.04 -P EAGLEChemistry:init_abundance_Hydrogen:0.74 -P EAGLEChemistry:init_abundance_Helium:0.23 -P COLIBREChemistry:init_abundance_metal:0.04 2>&1 | tee output_0p04.log
 
-python plot_box_evolution.py
+python3 plot_box_evolution.py
 
 ../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=4 stellar_evolution.yml -P EAGLEChemistry:init_abundance_metal:0.01 -P COLIBREChemistry:init_abundance_metal:0.01 2>&1 | tee output_0p01.log
 
-python plot_box_evolution.py
+python3 plot_box_evolution.py
 
 ../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=4 stellar_evolution.yml -P EAGLEChemistry:init_abundance_metal:0.001 -P COLIBREChemistry:init_abundance_metal:0.001 2>&1 | tee output_0p001.log
 
-python plot_box_evolution.py
+python3 plot_box_evolution.py
 
 ../../swift  --temperature --feedback --stars --hydro --external-gravity --threads=4 stellar_evolution.yml -P EAGLEChemistry:init_abundance_metal:0.0001 -P COLIBREChemistry:init_abundance_metal:0.0001 2>&1 | tee output_0p0001.log
 
-python plot_box_evolution.py
+python3 plot_box_evolution.py
