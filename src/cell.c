0f99bb2403aec6dc692c341173d27c50bf26dfe9
loikki <loic.hausammann@protonmail.ch>
2020-06-16 14:15:59 +0200

    Fromat

--- src/cell.c
+++ src/cell.c
@@ -4690,7 +4690,8 @@ void cell_drift_part(struct cell *c, const struct engine *e, int force) {
 #ifdef WITH_LOGGER
             if (e->policy & engine_policy_logger) {
               /* Log the particle a last time */
-              const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+              const uint32_t flag =
+                  logger_pack_flags_and_data(logger_flag_delete, 0);
               logger_log_part(e->logger, p, xp, e, /* log_all */ 1, flag);
             }
 #endif
@@ -4869,7 +4870,8 @@ void cell_drift_gpart(struct cell *c, const struct engine *e, int force) {
 #ifdef WITH_LOGGER
               if (e->policy & engine_policy_logger) {
                 /* Log the particle a last time */
-                const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+                const uint32_t flag =
+                    logger_pack_flags_and_data(logger_flag_delete, 0);
                 logger_log_gpart(e->logger, gp, e, /* log_all */ 1, flag);
               }
 #endif
@@ -5019,7 +5021,8 @@ void cell_drift_spart(struct cell *c, const struct engine *e, int force) {
 #ifdef WITH_LOGGER
             if (e->policy & engine_policy_logger) {
               /* Log the particle a last time */
-              const uint32_t flag = logger_pack_flags_and_data(logger_flag_delete, 0);
+              const uint32_t flag =
+                  logger_pack_flags_and_data(logger_flag_delete, 0);
               logger_log_spart(e->logger, sp, e, /* log_all */ 1, flag);
             }
 #endif
