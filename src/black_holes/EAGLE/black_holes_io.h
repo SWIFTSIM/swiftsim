5511092b65baef09a0f7499d1c36df8c5d6b84a7
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-07-08 18:54:37 +0200

    Fix a-factor dependence of the gas density around BHs

--- src/black_holes/EAGLE/black_holes_io.h
+++ src/black_holes/EAGLE/black_holes_io.h
@@ -178,7 +178,7 @@ INLINE static void black_holes_write_particles(const struct bpart* bparts,
   }
 
   list[7] = io_make_output_field(
-      "GasDensities", FLOAT, 1, UNIT_CONV_DENSITY, 0.f, bparts, rho_gas,
+      "GasDensities", FLOAT, 1, UNIT_CONV_DENSITY, -3.f, bparts, rho_gas,
       "Co-moving densities of the gas around the particles");
 
   list[8] = io_make_output_field(
