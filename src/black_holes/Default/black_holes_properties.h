137a1ef29a496c3b7cc46c930318692865c7f019
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-07-10 18:55:22 +0200

    Fix compilation problem due to the nibbling option when neither EAGLE nor COLIBRE is used

--- src/black_holes/Default/black_holes_properties.h
+++ src/black_holes/Default/black_holes_properties.h
@@ -47,6 +47,9 @@ struct black_holes_props {
 
   /*! Maximal change of h over one time-step */
   float log_max_h_change;
+
+  /*! Are we doing nibbling instead of swallowing? */
+  int use_nibbling;
 };
 
 /**
@@ -95,6 +98,9 @@ static INLINE void black_holes_props_init(struct black_holes_props *bp,
     bp->log_max_h_change = hydro_props->log_max_h_change;
   else
     bp->log_max_h_change = logf(powf(max_volume_change, hydro_dimension_inv));
+
+  /* No nibbling in the basic model */
+  bp->use_nibbling = 0;
 }
 
 /**
