274cb1728b1eeef4d6886f883d9e2b48a41908d0
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-07-10 15:10:44 +0200

    Initialise the EoS *after* the unit system

--- examples/main_fof.c
+++ examples/main_fof.c
@@ -388,12 +388,6 @@ int main(int argc, char *argv[]) {
   }
 #endif
 
-  /* Initialise the equation of state */
-  if (with_hydro)
-    eos_init(&eos, &prog_const, &us, params);
-  else
-    bzero(&eos, sizeof(struct eos_parameters));
-
   /* Prepare and verify the selection of outputs */
   io_prepare_output_fields(output_options, /*with_cosmology=*/1, /*with_fof=*/1,
                            /*with_structure_finding=*/0);
@@ -425,6 +419,12 @@ int main(int argc, char *argv[]) {
       params, "InitialConditions:cleanup_velocity_factors", 0);
   /* Initialise the cosmology */
   cosmology_init(params, &us, &prog_const, &cosmo);
+
+  /* Initialise the equation of state */
+  if (with_hydro)
+    eos_init(&eos, &prog_const, &us, params);
+  else
+    bzero(&eos, sizeof(struct eos_parameters));
 
   /* Initialise the FOF properties */
   bzero(&fof_properties, sizeof(struct fof_props));
