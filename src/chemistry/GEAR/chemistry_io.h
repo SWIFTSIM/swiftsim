157a5a4bc9f1181a3a5ec54a55ecbb5a94787b60
loikki <loic.hausammann@protonmail.ch>
2020-07-13 09:17:34 +0200

    Format

--- src/chemistry/GEAR/chemistry_io.h
+++ src/chemistry/GEAR/chemistry_io.h
@@ -20,8 +20,8 @@
 #define SWIFT_CHEMISTRY_IO_GEAR_H
 
 #include "chemistry_struct.h"
-#include "error.h"
 #include "engine.h"
+#include "error.h"
 #include "feedback.h"
 #include "io_properties.h"
 #include "parser.h"
