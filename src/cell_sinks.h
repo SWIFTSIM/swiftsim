2811711c6ff1dfe0694b9e21b2c5d16cc4038a5f
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2020-08-17 20:06:18 +0100

    Re-organise things a little in the cell structure

--- src/cell_sinks.h
+++ src/cell_sinks.h
@@ -17,31 +17,31 @@ struct cell_sinks {
     /*! Pointer to the #sink data. */
     struct sink *parts;
 
-    /*! Nr of #sink this cell can hold after addition of new one. */
-    int count_total;
-
-    /*! Is the #sink data of this cell being used in a sub-cell? */
-    int hold;
-
-    /*! Spin lock for various uses (#sink case). */
-    swift_lock_type lock;
-
     /*! Last (integer) time the cell's sink were drifted forward in time. */
     integertime_t ti_old_part;
-
-    /*! Minimum end of (integer) time step in this cell for sink tasks. */
-    integertime_t ti_end_min;
 
     /*! Maximum end of (integer) time step in this cell for sink tasks. */
     integertime_t ti_end_max;
 
-    /*! Maximum beginning of (integer) time step in this cell for sink
-     * tasks. */
-    integertime_t ti_beg_max;
-
 #ifdef SINK_NONE
   };
 #endif
+
+  /*! Minimum end of (integer) time step in this cell for sink tasks. */
+  integertime_t ti_end_min;
+    
+  /*! Maximum beginning of (integer) time step in this cell for sink
+   * tasks. */
+  integertime_t ti_beg_max;
+
+  /*! Spin lock for various uses (#sink case). */
+  swift_lock_type lock;
+
+  /*! Nr of #sink this cell can hold after addition of new one. */
+  int count_total;
+  
+  /*! Is the #sink data of this cell being used in a sub-cell? */
+  int hold;
 
   /*! Nr of #sink in this cell. */
   int count;
