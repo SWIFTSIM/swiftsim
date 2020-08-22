11183ffc98745f7281c9c946f538f33b73d3ccd2
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-08-22 22:31:33 +0200

    Moved the hydro-related cell fields to a separate structure

--- src/cell.h
+++ src/cell.h
@@ -34,6 +34,7 @@
 /* Local includes. */
 #include "align.h"
 #include "cell_black_holes.h"
+#include "cell_hydro.h"
 #include "cell_sinks.h"
 #include "cell_stars.h"
 #include "kernel_hydro.h"
@@ -345,137 +346,6 @@ struct cell {
   /*! Cell flags bit-mask. */
   volatile uint32_t flags;
 
-  /*! Hydro variables */
-  struct {
-
-    /*! Pointer to the #part data. */
-    struct part *parts;
-
-    /*! Pointer to the #xpart data. */
-    struct xpart *xparts;
-
-    /*! Pointer for the sorted indices. */
-    struct sort_entry *sort;
-
-    /*! Super cell, i.e. the highest-level parent cell that has a hydro
-     * pair/self tasks */
-    struct cell *super;
-
-    /*! The task computing this cell's sorts. */
-    struct task *sorts;
-
-    /*! The drift task for parts */
-    struct task *drift;
-
-    /*! Linked list of the tasks computing this cell's hydro density. */
-    struct link *density;
-
-    /* Linked list of the tasks computing this cell's hydro gradients. */
-    struct link *gradient;
-
-    /*! Linked list of the tasks computing this cell's hydro forces. */
-    struct link *force;
-
-    /*! Linked list of the tasks computing this cell's limiter. */
-    struct link *limiter;
-
-    /*! Dependency implicit task for the ghost  (in->ghost->out)*/
-    struct task *ghost_in;
-
-    /*! Dependency implicit task for the ghost  (in->ghost->out)*/
-    struct task *ghost_out;
-
-    /*! The ghost task itself */
-    struct task *ghost;
-
-    /*! The extra ghost task for complex hydro schemes */
-    struct task *extra_ghost;
-
-    /*! The task to end the force calculation */
-    struct task *end_force;
-
-    /*! Dependency implicit task for cooling (in->cooling->out) */
-    struct task *cooling_in;
-
-    /*! Dependency implicit task for cooling (in->cooling->out) */
-    struct task *cooling_out;
-
-    /*! Task for cooling */
-    struct task *cooling;
-
-    /*! Task for star formation */
-    struct task *star_formation;
-
-    /*! Task for sorting the stars again after a SF event */
-    struct task *stars_resort;
-
-    /*! Last (integer) time the cell's part were drifted forward in time. */
-    integertime_t ti_old_part;
-
-    /*! Minimum end of (integer) time step in this cell for hydro tasks. */
-    integertime_t ti_end_min;
-
-    /*! Maximum end of (integer) time step in this cell for hydro tasks. */
-    integertime_t ti_end_max;
-
-    /*! Maximum beginning of (integer) time step in this cell for hydro tasks.
-     */
-    integertime_t ti_beg_max;
-
-    /*! Spin lock for various uses (#part case). */
-    swift_lock_type lock;
-
-    /*! Max smoothing length in this cell. */
-    float h_max;
-
-    /*! Maximum part movement in this cell since last construction. */
-    float dx_max_part;
-
-    /*! Maximum particle movement in this cell since the last sort. */
-    float dx_max_sort;
-
-    /*! Values of h_max before the drifts, used for sub-cell tasks. */
-    float h_max_old;
-
-    /*! Values of dx_max before the drifts, used for sub-cell tasks. */
-    float dx_max_part_old;
-
-    /*! Values of dx_max_sort before the drifts, used for sub-cell tasks. */
-    float dx_max_sort_old;
-
-    /*! Nr of #part in this cell. */
-    int count;
-
-    /*! Nr of #part this cell can hold after addition of new #part. */
-    int count_total;
-
-    /*! Number of #part updated in this cell. */
-    int updated;
-
-    /*! Is the #part data of this cell being used in a sub-cell? */
-    int hold;
-
-    /*! Bit mask of sort directions that will be needed in the next timestep. */
-    uint16_t requires_sorts;
-
-    /*! Bit mask of sorts that need to be computed for this cell. */
-    uint16_t do_sort;
-
-    /*! Bit-mask indicating the sorted directions */
-    uint16_t sorted;
-
-    /*! Bit-mask indicating the sorted directions */
-    uint16_t sort_allocated;
-
-#ifdef SWIFT_DEBUG_CHECKS
-
-    /*! Last (integer) time the cell's sort arrays were updated. */
-    integertime_t ti_sort;
-
-#endif
-
-  } hydro;
-
   /*! Grav variables */
   struct {
 
@@ -569,6 +439,9 @@ struct cell {
     short int nr_mm_tasks;
 
   } grav;
+
+  /*! Hydro variables */
+  struct cell_hydro hydro;
 
   /*! Stars variables */
   struct cell_stars stars;
@@ -1226,11 +1099,13 @@ __attribute__((always_inline)) INLINE static void cell_malloc_hydro_sorts(
 __attribute__((always_inline)) INLINE static void cell_free_hydro_sorts(
     struct cell *c) {
 
+#ifndef NONE_SPH
   if (c->hydro.sort != NULL) {
     swift_free("hydro.sort", c->hydro.sort);
     c->hydro.sort = NULL;
     c->hydro.sort_allocated = 0;
   }
+#endif
 }
 
 /**
