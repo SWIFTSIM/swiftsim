d7cc14348e4571b1188cd6e76d0122ab3ee714bc
loikki <loic.hausammann@protonmail.ch>
2020-09-01 11:15:14 +0200

    Code compiles

--- src/task.h
+++ src/task.h
@@ -99,6 +99,7 @@ enum task_types {
   task_type_fof_pair,
   task_type_sink_in,  /* Implicit */
   task_type_sink_out, /* Implicit */
+  task_type_sink_formation,
   task_type_count
 } __attribute__((packed));
 
@@ -136,7 +137,7 @@ enum task_subtypes {
   task_subtype_do_gas_swallow,
   task_subtype_do_bh_swallow,
   task_subtype_bh_feedback,
-  task_subtype_sink,
+  task_subtype_sink_compute_formation,
   task_subtype_count
 } __attribute__((packed));
 
@@ -172,6 +173,7 @@ enum task_categories {
   task_category_mpi,
   task_category_fof,
   task_category_others,
+  task_category_sink_formation,
   task_category_count
 };
 
