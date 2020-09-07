322ffad706aa81fa9f7aa0f2036e84db4625271b
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-09-07 10:39:14 +0200

    Update the hydro check to better test the accuracy of the force calculation

--- src/engine.c
+++ src/engine.c
@@ -1763,7 +1763,8 @@ void engine_init_particles(struct engine *e, int flag_entropy_ICs,
 
 #ifdef SWIFT_HYDRO_DENSITY_CHECKS
   /* Check the accuracy of the hydro calculation */
-  if (e->policy & engine_policy_hydro) hydro_exact_density_check(e->s, e, 1e-3, 0);
+  if (e->policy & engine_policy_hydro)
+    hydro_exact_density_check(e->s, e, 1e-3, 0);
 #endif
 
   /* Apply some conversions (e.g. internal energy -> entropy) */
@@ -1855,7 +1856,8 @@ void engine_init_particles(struct engine *e, int flag_entropy_ICs,
 
 #ifdef SWIFT_HYDRO_DENSITY_CHECKS
   /* Check the accuracy of the hydro calculation */
-  if (e->policy & engine_policy_hydro) hydro_exact_density_check(e->s, e, 1e-3, 1);
+  if (e->policy & engine_policy_hydro)
+    hydro_exact_density_check(e->s, e, 1e-3, 1);
 #endif
 
 #ifdef SWIFT_GRAVITY_FORCE_CHECKS
@@ -2296,7 +2298,8 @@ void engine_step(struct engine *e) {
 
 #ifdef SWIFT_HYDRO_DENSITY_CHECKS
   /* Check the accuracy of the hydro calculation */
-  if (e->policy & engine_policy_hydro) hydro_exact_density_check(e->s, e, 1e-3, 1);
+  if (e->policy & engine_policy_hydro)
+    hydro_exact_density_check(e->s, e, 1e-3, 1);
 #endif
 
 #ifdef SWIFT_DEBUG_CHECKS
