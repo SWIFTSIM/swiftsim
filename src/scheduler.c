1aa2cdb1d45d89b96d9ce34c225c70978832f39f
loikki <loic.hausammann@protonmail.ch>
2020-10-27 09:03:09 +0100

    2nd try

--- src/scheduler.c
+++ src/scheduler.c
@@ -432,12 +432,12 @@ void scheduler_write_dependencies(struct scheduler *s, int verbose) {
     /* Are we dealing with a task at the top level? */
     if (is_ci_top && is_cj_top) {
       cur->task_in_level =
-          max(cur->task_in_level, (int)task_dependency_level_top);
+          max(cur->task_in_level, (unsigned int)task_dependency_level_top);
     }
     /* At the super level? */
     else if (is_ci_super && is_cj_super) {
       cur->task_in_level =
-          max(cur->task_in_level, (int)task_dependency_level_super);
+          max(cur->task_in_level, (unsigned int)task_dependency_level_super);
     }
     /* At the hydro/grav level? */
     else if (is_hydro_super || is_grav_super) {
@@ -449,7 +449,8 @@ void scheduler_write_dependencies(struct scheduler *s, int verbose) {
               "levels");
         }
         cur->task_in_level =
-            max(cur->task_in_level, (int)task_dependency_level_super_hydro);
+            max(cur->task_in_level,
+                (unsigned int)task_dependency_level_super_hydro);
       }
       if (is_grav_super) {
         if (cur->task_in_level == task_dependency_level_super_hydro) {
@@ -457,8 +458,8 @@ void scheduler_write_dependencies(struct scheduler *s, int verbose) {
               "A task seems to be at the same time at the grav and hydro "
               "levels");
         }
-        cur->task_in_level =
-            max(cur->task_in_level, (int)task_dependency_level_super_grav);
+        cur->task_in_level = max(
+            cur->task_in_level, (unsigned int)task_dependency_level_super_grav);
       }
     }
     /* At a random level? */
