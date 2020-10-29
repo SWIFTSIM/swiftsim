c6367e4749dbd34831ba40e677f8a62d4df1eb0f
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2020-10-29 22:17:33 +0100

    should be finished.

--- src/rt/debug/rt_iact.h
+++ src/rt/debug/rt_iact.h
@@ -116,6 +116,7 @@ __attribute__((always_inline)) INLINE static void runner_iact_rt_flux_common(
       pj->rt_data.calls_iact_transport += 1;
     }
   }
+}
 
 /**
  * @brief Flux calculation between particle i and particle j
