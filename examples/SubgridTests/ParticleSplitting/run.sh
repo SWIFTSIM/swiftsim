d2a981f40e67d99fdec6e4f8cf6ed6c34715896f
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-11-21 21:36:28 +0000

    Adds example for checking if particle splitting works.

---
+++ examples/SubgridTests/ParticleSplitting/run.sh
@@ -0,0 +1,11 @@
+#!bash
+
+if [ ! -e particleSplitting.hdf5 ]
+then
+    echo "Making initial conditions for the ParticleSplitting example"
+    python3 makeIC.py
+fi
+
+
+# A very simple invocation for this one...
+../../swift --hydro -t 4 particle_splitting.yml
