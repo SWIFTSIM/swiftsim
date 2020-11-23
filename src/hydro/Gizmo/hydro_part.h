a0e2ed68c55997e30cc19c8df6acd8c7f17c38d2
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2020-11-24 00:10:26 +0100

    added rt to gizmo

--- src/hydro/Gizmo/hydro_part.h
+++ src/hydro/Gizmo/hydro_part.h
@@ -23,6 +23,7 @@
 #include "chemistry_struct.h"
 #include "cooling_struct.h"
 #include "feedback_struct.h"
+#include "rt_struct.h"
 #include "star_formation_struct.h"
 #include "timestep_limiter_struct.h"
 #include "tracers_struct.h"
