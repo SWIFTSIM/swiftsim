794ae44d8825afd5697142dd96fa1da229e8cc1a
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2020-12-18 19:59:16 +0100

    only rank 0 writes task levels at zeroth step

--- src/engine.c
+++ src/engine.c
@@ -1825,7 +1825,7 @@ void engine_init_particles(struct engine *e, int flag_entropy_ICs,
 #endif
 
   scheduler_write_dependencies(&e->sched, e->verbose, e->step);
-  scheduler_write_task_level(&e->sched, e->step);
+  if (e->nodeID == 0) scheduler_write_task_level(&e->sched, e->step);
 
   /* Run the 0th time-step */
   TIMER_TIC2;
