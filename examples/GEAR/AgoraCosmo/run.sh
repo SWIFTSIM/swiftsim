008eb30149318b4459e7654407effdde7cd04d5e
loikki <loic.hausammann@protonmail.ch>
2021-01-28 15:00:55 +0100

    Small fixes

--- examples/GEAR/AgoraCosmo/run.sh
+++ examples/GEAR/AgoraCosmo/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # MUSIC binary
-music=~/music/Music
+music=~/music/MUSIC
 if test -f $music; then
     echo "Using the following version of MUSIC $music."
 else
     echo "MUSIC is not found."
+    exit
 fi
 
 echo "Generating the initial conditions"
@@ -15,4 +18,4 @@ echo "Converting the initial conditions into a SWIFT compatible format"
 python3 convert_ic.py
 
 echo "Running SWIFT"
-../../swift --cooling --feedback --cosmology  --limiter --sync --self-gravity --hydro --stars --star-formation --threads=24 zoom_in.yml 2>&1 | tee output.log
+../../swift --cooling --feedback --cosmology  --limiter --sync --self-gravity --hydro --stars --star-formation --threads=24 agora_cosmo.yml 2>&1 | tee output.log
