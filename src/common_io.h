a9c61aa868b0b0d4516a34472ab4a4baea21c05d
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2021-01-29 17:45:22 +0000

    Change the signature of the selection of gpart VR fields to accomodate the case where we do not yet have an engine pointer

--- src/common_io.h
+++ src/common_io.h
@@ -209,9 +209,11 @@ void io_select_hydro_fields(const struct part* const parts,
                             const struct engine* const e, int* const num_fields,
                             struct io_props* const list);
 
-void io_select_dm_fields(const struct gpart* const gparts, const int with_fof,
-                         const int with_stf, const struct engine* const e,
-                         int* const num_fields, struct io_props* const list);
+void io_select_dm_fields(const struct gpart* const gparts,
+                         const struct velociraptor_gpart_data* gpart_group_data,
+                         const int with_fof, const int with_stf,
+                         const struct engine* const e, int* const num_fields,
+                         struct io_props* const list);
 
 void io_select_sink_fields(const struct sink* const sinks,
                            const int with_cosmology, const int with_fof,
