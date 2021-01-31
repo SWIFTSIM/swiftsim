f7ccd5cfef8ff56d8734a9680afd02dbc985e8f2
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-01-31 17:07:10 +0100

    Add function to reset the gas particle-carried feedback-related fields in the gas drift

--- src/feedback/EAGLE_thermal/feedback.h
+++ src/feedback/EAGLE_thermal/feedback.h
@@ -46,8 +46,19 @@ void compute_stellar_evolution(const struct feedback_props* feedback_props,
  * @param e The #engine.
  */
 __attribute__((always_inline)) INLINE static void feedback_update_part(
-    struct part* restrict p, struct xpart* restrict xp,
-    const struct engine* restrict e) {}
+    struct part* p, struct xpart* xp, const struct engine* e) {}
+
+/**
+ * @brief Reset the gas particle-carried fields related to feedback at the
+ * start of a step.
+ *
+ * Nothing to do here in the EAGLE-thermal model.
+ *
+ * @param p The particle.
+ * @param xp The extended data of the particle.
+ */
+__attribute__((always_inline)) INLINE static void feedback_reset_part(
+    struct part* p, struct xpart* xp) {}
 
 /**
  * @brief Should this particle be doing any feedback-related operation?
