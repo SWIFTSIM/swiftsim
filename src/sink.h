800521c50ebbbc48b76c8c895d75581b0c68f281
loikki <loic.hausammann@protonmail.ch>
2021-02-17 19:07:08 +0100

    Add docstring

--- src/sink.h
+++ src/sink.h
@@ -22,6 +22,13 @@
 /* Config parameters. */
 #include "../config.h"
 
+/**
+ * @brief Defines which sink particle should be removed.
+ *
+ * In the iact function of the merger, we return this enum in order
+ * to define if the none, the first or the second sink particle should be
+ * removed (in the order of the arguments)
+ */
 enum sink_merger_remove {
   sink_merger_remove_none,
   sink_merger_remove_first,
