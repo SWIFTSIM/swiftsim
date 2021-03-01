d5625369896c53cf99149c1031af154da02e17b6
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-03-01 10:30:28 +0100

    Applied code formatting script

--- examples/main.c
+++ examples/main.c
@@ -1410,9 +1410,8 @@ int main(int argc, char *argv[]) {
                 &reparttype, &us, &prog_const, &cosmo, &hydro_properties,
                 &entropy_floor, &gravity_properties, &stars_properties,
                 &black_holes_properties, &sink_properties, &feedback_properties,
-		 &rt_properties,
-                &mesh, &potential, &cooling_func, &starform, &chemistry,
-                &fof_properties, &los_properties);
+                &rt_properties, &mesh, &potential, &cooling_func, &starform,
+                &chemistry, &fof_properties, &los_properties);
     engine_config(/*restart=*/0, /*fof=*/0, &e, params, nr_nodes, myrank,
                   nr_threads, with_aff, talking, restart_file);
 
