4fba6735c1dd4c4135c0c804d1d9929aa7768df9
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-03-06 16:35:10 +0100

    Hide all the cell-carried RT variables into a union to save memory.

--- src/cell_hydro.h
+++ src/cell_hydro.h
@@ -109,32 +109,35 @@ struct cell_hydro {
     /*! Task for sorting the stars again after a SF event */
     struct task *stars_resort;
 
-    /*! Radiative transfer ghost in task */
-    struct task *rt_in;
+    union {
 
-    /*! Task for self/pair injection step of radiative transfer */
-    struct link *rt_inject;
+      /*! Radiative transfer ghost in task */
+      struct task *rt_in;
 
-    /*! Radiative transfer ghost1 task (finishes up injection) */
-    struct task *rt_ghost1;
+      /*! Task for self/pair injection step of radiative transfer */
+      struct link *rt_inject;
 
-    /*! Task for self/pair gradient step of radiative transfer */
-    struct link *rt_gradient;
+      /*! Radiative transfer ghost1 task (finishes up injection) */
+      struct task *rt_ghost1;
 
-    /*! Radiative transfer ghost2 task */
-    struct task *rt_ghost2;
+      /*! Task for self/pair gradient step of radiative transfer */
+      struct link *rt_gradient;
 
-    /*! Task for self/pair transport step of radiative transfer */
-    struct link *rt_transport;
+      /*! Radiative transfer ghost2 task */
+      struct task *rt_ghost2;
 
-    /*! Radiative transfer transport out task */
-    struct task *rt_transport_out;
+      /*! Task for self/pair transport step of radiative transfer */
+      struct link *rt_transport;
 
-    /*! Radiative transfer thermochemistry task */
-    struct task *rt_tchem;
+      /*! Radiative transfer transport out task */
+      struct task *rt_transport_out;
 
-    /*! Radiative transfer ghost out task */
-    struct task *rt_out;
+      /*! Radiative transfer thermochemistry task */
+      struct task *rt_tchem;
+
+      /*! Radiative transfer ghost out task */
+      struct task *rt_out;
+    };
 
     /*! Last (integer) time the cell's part were drifted forward in time. */
     integertime_t ti_old_part;
