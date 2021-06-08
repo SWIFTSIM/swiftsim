73424790b3b0d1693348d6d0ad406cebc4168cf9
loikki <loic.hausammann@protonmail.com>
2021-06-08 08:02:35 +0200

    add ifdef

--- src/engine_collect_end_of_step.c
+++ src/engine_collect_end_of_step.c
@@ -498,9 +498,11 @@ void engine_collect_end_of_step(struct engine *e, int apply) {
   data.ti_black_holes_end_min = max_nr_timesteps,
   data.ti_black_holes_beg_max = 0, data.e = e, data.csds_file_size_gb = 0;
 
+#ifdef WITH_CSDS
   /* Get the file size from the CSDS. */
   if (e->policy & engine_policy_csds)
     data.csds_file_size_gb = csds_get_current_filesize_used_gb(e->csds, e);
+#endif
 
   /* Need to use a consistent check of the hours since we started. */
   data.runtime = clocks_get_hours_since_start();
