f5c1c17f331c426d2826151f1bc908303f446593
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-06-17 17:12:31 +0200

    Also call the new task function when enqueing a task that is done

--- src/scheduler.c
+++ src/scheduler.c
@@ -2323,6 +2323,7 @@ struct task *scheduler_done(struct scheduler *s, struct task *t) {
     if (res < 1) {
       error("Negative wait!");
     } else if (res == 1) {
+      task_pass_buffer(t, t2);
       scheduler_enqueue(s, t2);
     }
   }
